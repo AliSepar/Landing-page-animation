0edef2ef1d9aadd9d0c37315175b938e4762b771
@AliSepar <alisabah.separ1@gmail.com>
2020-09-23 23:59:40 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const t1 = gsap.timeline( {defaults: {ease: "power1.out"} } );
+
+t1.to('.text',{y:"0%",duration:1 ,stagger:0.25 });
+t1.to(".slider", {y:"-100%" , duration: 1.5, delay:0.6 });
+t1.to(".intro",{y:"-100%", duration:1}, "-=1");
+t1.fromTo("nav", {opacity: 0}, {opacity:1, duration:1});
+t1.fromTo(".big-text", {opacity: 0}, {opacity:1, duration:1}, '-=1');
